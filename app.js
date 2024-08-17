20a1a36816ef6016230df76f08d6abc5943c8857
skyboj <skyboj@yandex.ru>
2024-08-18 00:19:55 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const inputField = document.getElementById('synced-text');
+
+// Инициализация Replicache
+const replicache = new Replicache({
+    name: 'my-replicache',
+    mutators: {
+        updateText: async (_, { text }) => {
+            await replicache.put('text', text);
+        },
+    },
+});
+
+// Синхронизация текста
+async function syncText() {
+    const text = await replicache.get('text');
+    inputField.value = text || '';
+
+    inputField.addEventListener('input', async () => {
+        await replicache.mutate.updateText({ text: inputField.value });
+    });
+}
+
+syncText();
